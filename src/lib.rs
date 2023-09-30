65e7088f3c49fcbdc0d2fe4ae90ffe0ffce63173
Rachel Bousfield <nfranks@protonmail.com>
2023-09-29 23:03:13 -0600

    remove letter-digit boundaries

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 // Copyright 2023, Offchain Labs, Inc.
 // For licensing, see https://github.com/OffchainLabs/sneks/blob/stylus/licenses/COPYRIGHT.md
 
-use convert_case::{Case, Casing};
+use convert_case::{Boundary, Case, Casing};
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, Fields, ItemEnum};
@@ -18,7 +18,12 @@ pub fn derive_simple_snake_names(input: TokenStream) -> TokenStream {
     let mut arms = quote!();
     for variant in variants {
         let name = variant.ident;
-        let snek = name.to_string().to_case(Case::Snake);
+        let snek = name
+            .to_string()
+            .from_case(Case::Camel)
+            .without_boundaries(&Boundary::letter_digit())
+            .to_case(Case::Snake);
+
         arms.extend(match variant.fields {
             Fields::Unit => quote! { #name => #snek, },
             Fields::Unnamed(..) => quote! { #name(..) => #snek, },
